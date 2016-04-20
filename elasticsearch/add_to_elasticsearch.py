aa6aa9582272124381f60a772e0ac8b58a14bf39
Atul Nambudiri <Atul-Nambudiri@github.com>
2016-04-19 19:12:19 -0500

    Added in more to query

--- elasticsearch/add_to_elasticsearch.py
+++ elasticsearch/add_to_elasticsearch.py
@@ -1,4 +1,5 @@
 import json
+import subprocess
 from elasticsearch import Elasticsearch
 
 es = Elasticsearch(['198.199.84.154'])
@@ -7,8 +8,13 @@ fruitlist = {}
 with open('fruitlist.json') as json_file:
 	fruitlist = json.load(json_file)
 
+index = "fruit-index"
+
+subprocess.call("curl -XDELETE 'http://198.199.84.154:9200/%s/'" % index, shell=True)
+subprocess.call("curl -PUT 'http://198.199.84.154:9200/%s' -d '{ \"settings\": { \"analysis\": {\"filter\": { \"en_US\": {\"type\": \"hunspell\", \"language\": \"en_US\"}, \"english_stemmer\": {\"type\": \"stemmer\",\"language\": \"english\" },\"english_possessive_stemmer\": {\"type\": \"stemmer\", \"language\":  \"possessive_english\"}, \"my_synonym_filter\": {\"type\": \"synonym\", \"synonyms\": [ \"large,big\",\"small,tiny\"]},\"my_stop\": {\"type\" : \"stop\", \"stopwords\" : \"_english_\"}},\"analyzer\": {\"remove\": {\"tokenizer\": \"standard\",\"filter\": [\"english_possessive_stemmer\", \"lowercase\", \"my_stop\", \"my_synonym_filter\",\"en_US\"]}}}}}'" % index, shell=True)
+
 for key, value in fruitlist.iteritems():
-	res = es.index(index='fruit-index', doc_type="fruit-doc", body=value)
+	res = es.index(index="%s" % index, doc_type="fruit-doc", body=value)
 	print(res)
 
-es.indices.refresh()
+es.indices.refresh
--- elasticsearch/elasticsearch_query.py
+++ elasticsearch/elasticsearch_query.py
@@ -8,11 +8,13 @@ from elasticsearch import Elasticsearch
 
 es = Elasticsearch(['198.199.84.154'])
 
-res = es.search(index='fruit-index', body={
+res = es.search(index='fruit-index3', body={
 	"query" : {
 		"query_string" : {
 			"default_field" : "description",
-			"query" : "large red and green"
+			"fuzziness" : 2,
+			"query" : "large",
+			"analyzer" : "remove"
 		}
 	}
 })
@@ -22,7 +24,7 @@ for i in range(5):
 
 
 #This is the equivalent command line query
-# curl -XGET 'http://198.199.84.154:9200/fruit-index/_search' -d '{ "fields" : ["name"], "query": { "query_string" : {"default_field" : "description", "query": "large red and green" } } }'
+# curl -XGET 'http://198.199.84.154:9200/fruit-index/_search' -d '{ "fields" : ["name"], "query": { "query_string" : {"default_field" : "description", "fuzziness" : 2, "analyzer" : "remove", "query": "large red and green" } } }'
 
 #This query gets the full text of the passed in fruit. In this example it as a Black Fig
 # curl -XGET 'http://198.199.84.154:9200/fruit-index/_search' -d '{ "query": {"match" : {"name" : { "query" : "Black Fig", "operator": "and" }}} }'
