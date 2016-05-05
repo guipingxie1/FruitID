5a3f9a1852f46cee6d7e5a26afb68566a32665fb
Atul Nambudiri <Atul-Nambudiri@github.com>
2016-05-05 12:14:18 -0500

    Fixed grape

--- elasticsearch/elasticsearch_query.py
+++ elasticsearch/elasticsearch_query.py
@@ -24,7 +24,7 @@ for i in range(5):
 
 
 #This is the equivalent command line query
-# curl -XGET 'http://198.199.84.154:9200/fruit-index/_search' -d '{ "fields" : ["name"], "query": { "query_string" : {"default_field" : "description", "fuzziness" : 2, "analyzer" : "remove", "query": "large red and green" } } }'
+# curl -XGET 'http://198.199.84.154:9200/fruit-index/_search' -d '{ "fields" : ["name"], "query": { "query_string" : {"default_operator" : "and", default_field" : "description", "fuzziness" : 2, "analyzer" : "remove", "query": "large red and green" } } }'
 
 #This query gets the full text of the passed in fruit. In this example it as a Black Fig
 # curl -XGET 'http://198.199.84.154:9200/fruit-index/_search' -d '{ "query": {"match" : {"name" : { "query" : "Black Fig", "operator": "and" }}} }'
